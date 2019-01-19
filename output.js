e3fd46469173c820ea03c52332b727cc6a671479
Dailenis Gonzalez <dgonzalez@lionix.org>
2019-01-18 18:58:21 -0600

    feat: typescript notes

--- output.js
+++ output.js
@@ -16,43 +16,101 @@ var __extends = (this && this.__extends) || (function () {
  tsc output.ts
  tsc output.ts -w es igual a  tsc output.ts -watch Starting compilation in watch mode...
 */
-var myString = "Dai";
-var myAnyVar = "Soy cualquiera";
-myAnyVar = true;
-var myNumberArray = [1, 2];
-var myAnyArray = ["1", 2, true, undefined];
-//tuple
-var myArrayTuple = ["1", 2];
-var myVoidVar = null;
-//FUNCIONES
-function suma(num1, num2) {
-    //return num1+num2
-    var str = num1 + " - " + num2;
-    console.log("str", str);
+console.log("HOLA MUNDO :)");
+//En typescript no se puede cambiar el tipo de dato
+//myString = 2; //era un string no le puedo asignar un 2 error TS2322: Type '2' is not assignable to type 'string'.
+//Tipos de Datos
+var myString = "Dalenis";
+var myNumber = 30;
+var myBool = true;
+var myVarAny = "Puedo ser cualquier tipo de dato";
+var myArrayAny = [1, 2, 3, "", true]; // any[] Con esto le digo que este arreglo puede contener cualquier tipo de dato string number...
+var myArrayString = ["Item1", "Item2"]; //string[] con esto le digo que el arreglo solo puede contener strings
+document.write(myString);
+document.write(myNumber.toString());
+//Enums
+// Valores de tipo número comienza por defecto en 0 uy va aumentando el contador si se quiere que empiece con otro valor seteaar
+var Color;
+(function (Color) {
+    Color[Color["Green"] = 0] = "Green";
+    Color[Color["Red"] = 1] = "Red";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+//Obtener el valor del enum
+var blue = Color.Blue;
+console.log("Enum Blue: ", blue);
+// Obtener el nombre del enum
+var myVar = Color[1];
+//let myVar: string = Color[Color.Blue];
+console.log("myVar: ", myVar);
+//Convetir un número en un string
+var x = "hola :)";
+var y = 2;
+var numStr = String(y);
+console.log("numStr: ", typeof (numStr));
+//Tuple - una tupla es una lista ordenada de elementos
+var myStrNumTuple = ["Hola", 30]; //Solo puedo asignarle al arreglo el primer valor tipo string el segundo tipo numero
+document.write(typeof myStrNumTuple);
+//void, undefined, null
+var myVoid = undefined; //void es para definir tipos de datos vacios
+var myNull = null; // puede contener null , undefined
+var myUndefined = undefined;
+//Funciones
+//Le puedo decir a los parametros que tipo de datos son y a la función que tipo de dato tiene que retornar
+function getSum(num1, num2) {
+    return num1 + num2;
 }
-console.log(suma("2"));
-function mostrarTareas(tarea) {
-    console.log(tarea.tittle + " " + tarea.text);
+//getSum(1, "1");
+//Le puedo definir varios tipos de datos a la vez por ejemplo que puede recibir un numero o un string
+var getSum2 = function (num1, num2) {
+    if (typeof num1 === "string") {
+        num1 = parseInt(num1);
+    }
+    if (typeof num2 === "string") {
+        num2 = parseInt(num2);
+    }
+    return num1 + num2;
+};
+getSum2(1, "1");
+//Definir que un parámetros es opcional es con el signo de interogación ?
+function getName(firstname, lastname) {
+    return firstname + " " + lastname;
 }
-var myTarea = { tittle: "Typescript", text: "Aprendiendo interfaces" };
-mostrarTareas(myTarea);
-//mostrarTareas({tittle:"Typescript", text:"Aprendiendo interfaces" });
-//CLASES
+getName("Dailenis");
+//Función que no retorna nada void
+function myVoidFunction() {
+    console.log("myVoidFunction");
+}
+function showTodo(todo) {
+    console.log(todo.tittle + " - " + todo.text);
+}
+var myTodo = { tittle: "Hola", text: "Mundo :)" };
+showTodo({ tittle: "Typescript", text: "Aprendiendo interfaces" });
+//Clases
 var User = /** @class */ (function () {
     function User(name, email, age) {
         this.name = name;
         this.email = email;
         this.age = age;
     }
-    User.prototype.getAge = function () {
-        return this.age;
+    User.prototype.register = function () {
+        console.log(this.name + " is registered!");
+    };
+    User.prototype.paidInvoice = function () {
+        console.log(this.name + " tiene la memberes\u00EDa paga.");
+    };
+    User.prototype.ageInYears = function () {
+        return this.age + " years";
     };
     return User;
 }());
-var Usuario1 = new User("Jhon", "jhon@gmail.com", 32);
-console.log(Usuario1.name);
-//console.log(Usuario1.getAge());
-//HERENCIA
+//Instanciar la clase
+var User1 = new User("Jhon", "john@gmail.com", 32);
+User1.register();
+console.log(User1.email);
+//console.log(User1.age); No puedo acceder a la edad  porque no es un dato público
+console.log(User1.ageInYears());
+//Herencia
 var Member = /** @class */ (function (_super) {
     __extends(Member, _super);
     function Member(id, name, email, age) {
@@ -60,10 +118,8 @@ var Member = /** @class */ (function (_super) {
         _this.id = id;
         return _this;
     }
-    Member.prototype.getAgeFromChild = function () {
-        _super.prototype.getAge.call(this);
+    Member.prototype.payInvoice = function () {
+        _super.prototype.paidInvoice.call(this);
     };
     return Member;
 }(User));
-var Miembro1 = new Member(1, "Jhon", "jhon@gmail.com", 32);
-console.log("Desde el hijo: ", Miembro1.getAge());
